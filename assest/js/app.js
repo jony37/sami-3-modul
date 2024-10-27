f19f77990626bb15fc2bfa586427c90749a867d6
jony <ravshanovjony7@gmail.com>
2024-10-27 11:39:22 +0500

    51 dars JSON chuqur clonlash

--- assest/js/app.js
+++ assest/js/app.js
@@ -3,3 +3,29 @@
 
 // 50 dars Winga XAMPP yuklash
 ""
+
+// 51 JSON Chuqur clonlash
+
+const car = {
+    name: "BMW",
+    color: "red",
+    extra: {
+        isAirbag: false,
+        balon: 5
+    }
+}
+
+
+// objecti jsonga ugirib beradi
+const objTOJson = JSON.stringify(car);
+console.log(objTOJson);
+
+
+// Jsondi objctga ugirib beradi
+const JsonToObj = JSON.parse(objTOJson);
+console.log(JsonToObj);
+
+
+// objecti chuqur clonlash
+const clone = JSON.parse(JSON.stringify(car))
+console.log(clone );
